c0ea61ae4437b519e898bb1e39d80a08374c4f5d
Larry Naman <lawrence.naman@gmail.com>
2015-05-09 10:54:00 +0100

    removes unused data array

--- app/assets/scripts/config/common.js
+++ app/assets/scripts/config/common.js
@@ -4,23 +4,6 @@ define(function () {
 
     'use strict';
 
-    /*var data = [
-        {
-            title: 'Home',
-            name: 'home',
-            content: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-            active: true
-        }, {
-            title: 'About',
-            name: 'about',
-            content: 'Donec blandit purus ac felis tempus venenatis. Vestibulum laoreet augue vitae dui placerat, id mollis lectus aliquet. Curabitur rutrum tincidunt posuere. Aenean ex justo, viverra condimentum molestie ut, vulputate in nibh. Donec varius metus sit amet tincidunt mollis. Suspendisse quis pretium arcu. Nam faucibus, orci placerat vestibulum feugiat, leo orci ullamcorper nisi, vitae elementum lorem ante vitae tortor. Morbi sit amet felis dolor. Nullam eu est eget diam sodales pellentesque a eu leo. Aliquam eleifend luctus enim vel sodales. Nulla congue eu enim nec sodales. Quisque pellentesque purus eget maximus viverra. Sed iaculis gravida mauris vel tristique.'
-        }, {
-            title: 'Contact',
-            name: 'contact',
-            content: 'Duis dapibus, neque gravida cursus sagittis, enim est finibus diam, ac aliquam magna massa ac dolor. Sed aliquet, elit sodales volutpat lobortis, nisi sem condimentum nunc, pulvinar consequat lacus elit non mauris. Praesent semper lobortis diam, in mollis ex imperdiet id. Nulla malesuada lectus vitae malesuada laoreet. Phasellus faucibus odio id semper tincidunt. Aliquam ut mi mollis, lacinia sem quis, condimentum mi. Etiam malesuada posuere turpis, id efficitur ex gravida ut. Curabitur eu sollicitudin elit. Praesent tempus, est eget sodales finibus, felis erat mattis lorem, quis mollis nibh erat vel nulla. Proin enim leo, elementum sed urna nec, luctus pulvinar leo. Integer risus urna, lobortis quis dictum quis, volutpat nec ante. Suspendisse rhoncus eros at felis vulputate, egestas eleifend ex vehicula.'
-        }
-    ];*/
-
     return {
         baseTitle : 'Backbone Marionette Boilerplate',
         root : ''
